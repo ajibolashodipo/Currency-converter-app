a84d618fd6a003196dc9e52c47a1bc396ce390aa
ajibolashodipo <shodipoajibola@gmail.com>
2020-04-30 17:25:45 +0100

    greatly optimized code for speed esp when currencies are not Naira

--- app.js
+++ app.js
@@ -27,18 +27,22 @@ app.get("/convert", async (req, res) => {
     let sourceCurr = req.query.mc1;
     let destCurr = req.query.mc2;
     let sourceNaira = req.query.mc1;
-    let valued = await usdToNaira();
-    let rateNaira = parseInt(valued.conversionRate);
     let destNaira = req.query.mc2;
-    let nairaToDollar = rateNaira || 389.7;
+    let final;
+    //ensures that all non-naira requests do not go through the web scraper function
+    let isNaira = false;
 
     //check if source currency is Naira
     if (sourceCurr === "NGN") {
       sourceCurr = "USD";
+      //for the rainy day to come below
+      isNaira = true;
     }
     // check if destination currency is Naira
     if (destCurr === "NGN") {
       destCurr = "USD";
+      //for the rainy day to come below
+      isNaira = true;
     }
 
     //make and await the http request
@@ -48,23 +52,42 @@ app.get("/convert", async (req, res) => {
     //conversion rate
     let converted = await response.data.conversion_rates[destCurr];
 
-    //convert to naira since API does not support Naira
-    if (sourceNaira === "NGN") {
-      convertedTemp = converted / nairaToDollar;
-      converted = convertedTemp;
-    }
-    //convert to naira since API does not support Naira
-    if (destNaira === "NGN") {
-      convertedTemp = converted * nairaToDollar;
-      converted = convertedTemp;
-    }
-    //Final value
-    let tempFinal = converted * sourceVal;
-    let final = tempFinal.toFixed(2);
+    async function myKudi() {
+      //skip the webscraping if both source and destination are in naira
+      if (sourceNaira === "NGN" && destNaira === "NGN") {
+        converted = 1;
+        return converted;
+      }
+      //if either source or destination is naira, scrape the damn web
+      let valued = await usdToNaira();
+      let rateNaira = parseInt(valued.conversionRate);
+      let nairaToDollar = rateNaira || 389.7;
 
-    //this solves the floating point problem should source and destination be the same
-    if (sourceNaira === "NGN" && destNaira === "NGN") {
-      final = sourceVal;
+      //convert to naira since API does not support Naira
+      if (sourceNaira === "NGN") {
+        convertedTemp = converted / nairaToDollar;
+        converted = convertedTemp;
+        console.log(converted);
+        return converted;
+      }
+
+      //convert to naira since API does not support Naira
+      if (destNaira === "NGN") {
+        convertedTemp = converted * nairaToDollar;
+        converted = convertedTemp;
+        console.log(converted);
+        return converted;
+      }
+    }
+
+    //if either source or destination or both are naira
+    if (isNaira) {
+      let converted = await myKudi();
+      let tempFinal = converted * sourceVal;
+      final = tempFinal.toFixed(2);
+    } else {
+      let tempFinal = converted * sourceVal;
+      final = tempFinal.toFixed(2);
     }
 
     res.send({ final: final });
