cf8067184a15506721db9a66d9872fc572281f77
ajibolashodipo <shodipoajibola@gmail.com>
2020-05-11 13:41:10 +0100

    changed keypress event to keydown to accomodate for backspace and tab key presses

--- public/app.js
+++ public/app.js
@@ -1,31 +1,31 @@
-let sourceVal = document.getElementById("sourceVal");
-let myCurrency2 = document.getElementById("my-currency-2");
-let myCurrency1 = document.getElementById("my-currency-1");
-let destiVal = document.getElementById("destinationVal");
-let firstSubmit = document.getElementById("first-submit");
-var myForm = document.getElementById("form-1");
+let sourceVal = document.getElementById("sourceVal")
+let myCurrency2 = document.getElementById("my-currency-2")
+let myCurrency1 = document.getElementById("my-currency-1")
+let destiVal = document.getElementById("destinationVal")
+let firstSubmit = document.getElementById("first-submit")
+var myForm = document.getElementById("form-1")
 
-sourceVal.addEventListener("keypress", (e) => {
-  destiVal.value = "";
-});
+sourceVal.addEventListener("keydown", (e) => {
+  destiVal.value = ""
+})
 
 firstSubmit.addEventListener("click", (e) => {
   //shows loading on the screen while http request is being made
-  destiVal.value = "Loading...";
-  let sV = sourceVal.value;
-  let dV = destiVal.value;
-  let mc1 = myCurrency1.value;
-  let mc2 = myCurrency2.value;
+  destiVal.value = "Loading..."
+  let sV = sourceVal.value
+  let dV = destiVal.value
+  let mc1 = myCurrency1.value
+  let mc2 = myCurrency2.value
 
-  e.preventDefault();
+  e.preventDefault()
 
   fetch(`/convert?sv=${sV}&mc1=${mc1}&mc2=${mc2}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error);
+        console.log(data.error)
       } else {
-        destiVal.value = data.final;
+        destiVal.value = data.final
       }
-    });
-  });
-});
+    })
+  })
+})
